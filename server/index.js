291cc85891fd7e37b592f1ae7b093f7d90a815f3
afnasser49 <aly.nasser@gmail.com>
2026-05-31 13:04:22 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -53,7 +53,27 @@ function envUsers() {
 }
 
 function clean(obj = {}) {
-  return Object.fromEntries(Object.entries(obj).filter(([, value]) => value !== undefined && value !== ''));
+  return Object.fromEntries(
+    Object.entries(obj).filter(([, value]) => value !== undefined && value !== '')
+  );
+}
+
+function normalizeEmptyStrings(value) {
+  if (value === '') return undefined;
+
+  if (Array.isArray(value)) {
+    return value.map(normalizeEmptyStrings);
+  }
+
+  if (value && typeof value === 'object') {
+    return Object.fromEntries(
+      Object.entries(value)
+        .map(([key, val]) => [key, normalizeEmptyStrings(val)])
+        .filter(([, val]) => val !== undefined)
+    );
+  }
+
+  return value;
 }
 
 function sign(value) {
@@ -123,6 +143,14 @@ app.use(helmet({ contentSecurityPolicy: false }));
 app.use(compression());
 app.use(morgan('tiny'));
 app.use(express.json({ limit: '2mb' }));
+
+app.use((req, _res, next) => {
+  if (req.body && typeof req.body === 'object') {
+    req.body = normalizeEmptyStrings(req.body);
+  }
+  next();
+});
+
 app.use(auth);
 
 app.get('/api/health', (_req, res) => res.json({ ok: true }));
